fd230566d01d9c14e64900d114b1d3cadf80f744
chinnaiahgurram <chinnaiah.gurram@gmail.com>
2023-06-13 04:51:57 +0000

    React-Router

---
+++ src/components/About/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import './index.css'
+
+const About = () => (
+  <div className="about-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      alt="about"
+      className="about-image"
+    />
+    <h1 className="about-heading">About</h1>
+    <p className="about-paragraph">I Love to create! Iam Frontend Developer</p>
+  </div>
+)
+export default About
